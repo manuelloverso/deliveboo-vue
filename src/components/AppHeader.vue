83fc4365c1580017f635f4d518521fdf2b594dbf
Manuel <manuelloverso60@gmail.com>
2024-06-18 15:34:22 +0200

    add app header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,8 +7,50 @@ export default {
 };
 </script>
 <template>
-  <header>
-    <h1>i am the header</h1>
+  <header class="py-3 px-5">
+    <nav class="d-flex justify-content-between align-items-center">
+      <div class="logo">
+        <a href="#">
+          <img src="/public/img/deliverome-circledark-logo.svg" alt="" />
+        </a>
+      </div>
+      <ul class="user-links list-unstyled d-flex gap-4 m-0">
+        <li><a href="#">Login</a></li>
+        <li><a href="#">Registrati</a></li>
+      </ul>
+    </nav>
   </header>
 </template>
-<style scoped></style>
+<style scoped>
+header {
+  background-color: var(--bg-header);
+
+  .logo img {
+    display: block;
+    width: 100px;
+    transition: transform 0.2s ease;
+    &:hover {
+      transform: scale(1.1);
+    }
+  }
+
+  .user-links {
+    & li {
+      & a {
+        text-decoration: none;
+        color: var(--accent);
+        display: block;
+        padding: 7px 13px;
+        background-color: white;
+        border-radius: 10px;
+        transition: background-color 0.3s ease, color 0.3s ease;
+
+        &:hover {
+          background-color: var(--accent);
+          color: white;
+        }
+      }
+    }
+  }
+}
+</style>
