47e101b0ed28ba1aa10a7e6593f538ff9dad940c
Manuel <manuelloverso60@gmail.com>
2024-06-19 17:04:41 +0200

    add show view for single restaurant

--- src/router.js
+++ src/router.js
@@ -2,10 +2,14 @@ import { createWebHistory, createRouter } from "vue-router";
 
 /* Views */
 import HomeView from "./views/HomeView.vue";
+import RestaurantShow from "./views/RestaurantShow.vue";
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [{ path: "/", name: "home", component: HomeView }],
+  routes: [
+    { path: "/", name: "home", component: HomeView },
+    { path: "/restaurants/:id", component: RestaurantShow },
+  ],
 });
 
 export default router;
