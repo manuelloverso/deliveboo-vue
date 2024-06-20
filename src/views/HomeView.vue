6256c1ab8a314813a3154ff3bb914f60b837ed64
Manuel <manuelloverso60@gmail.com>
2024-06-20 09:53:16 +0200

    bug fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -63,68 +63,49 @@ export default {
 };
 </script>
 <template>
-
   <!-- Jumbotron -->
   <div class="jumbotron">
     <div class="overlay">
-
-
-
-
       <header class="py-3 px-5">
-    <nav class="d-flex justify-content-between align-items-center">
-      <div class="logo">
-        <a href="http://localhost:5173/">
-          <img src="/public/img/deliverome-circledark-logo.svg" alt="" />
-        </a>
-      </div>
-      <ul class="user-links list-unstyled d-flex gap-4 m-0">
-        <li><a href="http://127.0.0.1:8000/login">Login</a></li>
-        <li><a href="http://127.0.0.1:8000/register">Registrati</a></li>
-      </ul>
-    </nav>
-  </header>
+        <nav class="d-flex justify-content-between align-items-center">
+          <div class="logo">
+            <a href="http://localhost:5173/">
+              <img src="/public/img/deliverome-circledark-logo.svg" alt="" />
+            </a>
+          </div>
+          <ul class="user-links list-unstyled d-flex gap-4 m-0">
+            <li><a href="http://127.0.0.1:8000/login">Login</a></li>
+            <li><a href="http://127.0.0.1:8000/register">Registrati</a></li>
+          </ul>
+        </nav>
+      </header>
 
-
-
-
-
-
-      <div class="container text-white d-flex flex-column align-items-center justify-content-center h-50">
-
-
-        <h2>Benvenuto su Deliverome</h2> <br>
+      <div
+        class="container text-white d-flex flex-column align-items-center justify-content-center h-50"
+      >
+        <h2>Benvenuto su Deliverome</h2>
+        <br />
         <h3>Il miglior cibo della capitale direttamente a casa tua</h3>
-
 
         <h2>Sei un ristoratore?</h2>
         <a class="btn-jum" href="http://127.0.0.1:8000/register">Registrati</a>
-
-
       </div>
     </div>
-
-
   </div>
 
-  <div class="container ">
+  <div class="container">
     <!-- Types Filter -->
     <h2 class="text-center mt-4">Cosa vuoi mangiare oggi?</h2>
     <p class="text-center">Scegli una o più tipologie di ristorante</p>
     <div class="types-container">
-
-      <div :id="singleType.name" class="badge fs-6 type" role="button" data-bs-toggle="button"
-        v-for="(singleType, index) in store.types" @click="filterByTypes(index)">
-
       <div
         :id="singleType.name"
-        class="btn btn-primary"
+        class="badge fs-6 type"
         role="button"
         data-bs-toggle="button"
         v-for="(singleType, index) in store.types"
         @click="filterByTypes(index)"
-      
-
+      >
         {{ singleType.name }}
       </div>
     </div>
@@ -141,28 +122,29 @@ export default {
       </div>
     </div>
 
-    <template v-if="(activeTypes.length == 0)">
+    <template v-if="activeTypes.length == 0">
       <div class="container d-flex align-items-center gap-4 my-4">
         <div class="w-50 d-flex justify-content-center">
-          <img  src="/public/img/deliverome-circledark-logo.svg" alt="">
+          <img src="/public/img/deliverome-circledark-logo.svg" alt="" />
         </div>
         <div class="w-50">
           <h4>Chi siamo</h4>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis enim atque reprehenderit explicabo rerum delectus vero a excepturi sapiente fuga natus sint tempore obcaecati dolore ea omnis culpa, eveniet quod!</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis
+            enim atque reprehenderit explicabo rerum delectus vero a excepturi
+            sapiente fuga natus sint tempore obcaecati dolore ea omnis culpa,
+            eveniet quod!
+          </p>
         </div>
       </div>
+    </template>
 
     <template v-if="activeTypes.length == 0">
       <h1>test</h1>
-
     </template>
   </div>
 </template>
 <style scoped>
-
-
-
-
 header {
   /* background-color: var(--bg-header); */
 
@@ -195,15 +177,6 @@ header {
   }
 }
 
-
-
-
-
-
-
-
-
-
 .jumbotron {
   height: 80vh;
   background-color: red;
@@ -217,7 +190,6 @@ header {
   }
 
   .overlay {
-
     width: 100%;
     height: 100%;
     z-index: 1;
@@ -237,7 +209,6 @@ header {
       color: white;
     }
   }
-
 }
 
 .types-container {
@@ -247,9 +218,6 @@ header {
   gap: 1rem;
   margin: 3rem 0;
 
-
-
-
   .type {
     cursor: pointer;
     background-color: var(--accent);
@@ -258,14 +226,12 @@ header {
     &:hover {
       transform: scale(1.07);
       background-color: red;
-
     }
   }
 
   .active-type {
     background-color: red;
     transform: scale(1.07);
-
   }
 }
 </style>
