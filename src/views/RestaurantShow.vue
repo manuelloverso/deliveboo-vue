a654796443aecc58eb04df014398aeec0ac37acb
GioMench <giorgiaglm@gmail.com>
2024-06-24 14:52:33 +0200

    fix style restaurant show

--- src/views/RestaurantShow.vue
+++ src/views/RestaurantShow.vue
@@ -135,12 +135,12 @@ export default {
           </div>
         </div>
 
-        <div class="restaurant-info">
+        <div class="restaurant-info my-5">
           <!-- Image -->
           <img v-if="restaurant.image.startsWith('http')" class="restaurant-img" :src="restaurant.image" alt=""/>
           <img v-else class="restaurant-img" :src="'http://127.0.0.1:8000' + '/storage/' + restaurant.image" alt=""/>
 
-          <div class="text-left px-4">
+          <div class="text-left px-4 mb-4">
             <h2 class="fw-bold fs-1">{{ restaurant.restaurant_name }}</h2>
             <div class="d-flex gap-1 felx-wrap">
               <div v-for="restaurantType in restaurant.types" class="types_restaurant fw-semibold mb-4">
@@ -161,7 +161,7 @@ export default {
         <!-- Piatti -->
         <div class="restaurant-plates px-4 py-1 my-5">
           <template v-if="plates.length > 0">
-            <h2>Piatti</h2>
+            <h2 class="mb-3">Piatti</h2>
             <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4">
               <template v-for="plate in plates">
                 <div v-if="plate.is_visible == true" class="col pb-4">
