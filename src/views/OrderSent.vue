f851b0dc7f74c05dad9134679b8459daec38d15b
GioMench <giorgiaglm@gmail.com>
2024-06-25 16:42:39 +0200

    style ordersent

--- src/views/OrderSent.vue
+++ src/views/OrderSent.vue
@@ -7,42 +7,43 @@ export default {
 };
 </script>
 <template>
-  <main>
-    <div class="d-flex flex-column">
-      <h1 class="text-white">
-        Ordine effettuato con successo! <br />
-        Sarà a casa tua in pochi minuti
-      </h1>
-      <RouterLink :to="{ name: 'home' }" class="my-btn text-decoration-none"
+  <main class="ordersent-main">
+    <div class="d-flex flex-column text-center text-white">
+      <h1 class="fw-bold mb-4">Ordine effettuato con successo! </h1>
+         <h2>Sarà a casa tua in pochi minuti</h2>
+        
+     
+      <RouterLink :to="{ name: 'home' }" class="my-btn text-decoration-none my-3 "
         >Continua ad ordinare</RouterLink
       >
     </div>
   </main>
 </template>
 <style scoped>
-main {
+.ordersent-main {
   display: flex;
   align-items: center;
   justify-content: center;
   height: 60vh;
-  background: linear-gradient(
-    90deg,
-    rgba(250, 89, 0, 1) 9%,
-    rgba(255, 213, 0, 1) 100%
-  );
-
+  background: rgb(253,126,20);
+  background: linear-gradient(0deg, rgba(253,126,20,1) 0%, rgba(252,196,25,1) 100%);
+ 
+  & h1{
+    font-size: 50px;
+  }
+  
   .my-btn {
+    
     display: block;
     margin: auto;
     padding: 15px;
-    background-color: var(--accent);
     border: none;
     border-radius: 10px;
-    color: white;
+    color: white; 
     font-weight: 600;
-    transition: background-color 0.3s ease;
+    transition: background-color 0.98s ease;
     &:hover {
-      background-color: #e8590c;
+      font-size: 15px;
     }
   }
 }
