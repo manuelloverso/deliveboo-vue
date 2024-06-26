659c701deb501c14c9a190cb5edda95514c3152e
GioMench <giorgiaglm@gmail.com>
2024-06-26 12:05:23 +0200

    fix responsive of checkout vue

--- src/views/CheckoutView.vue
+++ src/views/CheckoutView.vue
@@ -110,195 +110,129 @@ export default {
 <template>
   <main class="checkout-main">
     <div class="container py-4 d-flex justify-content-between text-white">
-      <div class="col-5 p-4">
-        <template v-if="store.cart.length > 0">
-          <div class="cart">
-            <div
-              class="header d-flex justify-content-between align-items-center"
-            >
-              <div class="info">
-                <h2 class="mb-3">Riepilogo</h2>
-                <h4>{{ restaurant_name }}</h4>
+      <div class="row">
+        <div class="col-12 col-lg-6 p-4">
+          <template v-if="store.cart.length > 0">
+            <div class="cart">
+              <div class="header d-flex justify-content-between align-items-center">
+                <div class="info">
+                  <h2 class="mb-3">Riepilogo</h2>
+                  <h4>{{ restaurant_name }}</h4>
+                </div>
+                <button @click="store.emptyCart()" class="btn btn-danger">
+                  <i class="fa-solid fa-trash"></i>
+                </button>
               </div>
-              <button @click="store.emptyCart()" class="btn btn-danger">
-                <i class="fa-solid fa-trash"></i>
-              </button>
-            </div>
-            <div class="items">
-              <div
-                class="item d-flex justify-content-between"
-                v-for="item in store.cart"
-              >
-                <div class="pic-name">
-                  <!-- Image -->
-                  <template v-if="item.plateObj.image != null">
-                    <img
-                      loading="lazy"
-                      v-if="item.plateObj.image.startsWith('http')"
-                      class="plate-img"
-                      :src="item.plateObj.image"
-                      alt=""
-                    />
-                    <img
-                      loading="lazy"
-                      v-else
-                      class="plate-img"
-                      :src="
-                        'http://127.0.0.1:8000' +
+              <div class="items">
+                <div class="item d-flex justify-content-between" v-for="item in store.cart">
+                  <div class="pic-name">
+                    <!-- Image -->
+                    <template v-if="item.plateObj.image != null">
+                      <img loading="lazy" v-if="item.plateObj.image.startsWith('http')" class="plate-img"
+                        :src="item.plateObj.image" alt="" />
+                      <img loading="lazy" v-else class="plate-img" :src="'http://127.0.0.1:8000' +
                         '/storage/' +
                         item.plateObj.image
-                      "
-                      alt=""
-                    />
-                  </template>
-                  <img
-                    loading="lazy"
-                    v-else
-                    class="plate-img"
-                    src="/public/img/plate-default.jpg"
-                    alt=""
-                  />
+                        " alt="" />
+                    </template>
+                    <img loading="lazy" v-else class="plate-img" src="/public/img/plate-default.jpg" alt="" />
 
-                  <!-- Plate Name -->
-                  <span>
-                    <strong>{{ item.plateObj.name }}</strong></span
-                  >
-                </div>
-                <!-- actions -->
-                <div class="actions-btns d-flex gap-2 align-items-center me-2">
-                  <strong>{{ sumPrice(item) }}€</strong>
-                  <button
-                    class="btn btn-dark"
-                    @click="store.addPlate(item.plateObj, restaurant_name)"
-                  >
-                    <i class="fa-solid fa-plus"></i>
-                  </button>
-                  <span>{{ item.quantity }}x</span>
-                  <button
-                    class="btn btn-dark"
-                    @click="store.removePlate(item.plateObj)"
-                  >
-                    <i class="fa-solid fa-minus"></i>
-                  </button>
+                    <!-- Plate Name -->
+                    <span>
+                      <strong>{{ item.plateObj.name }}</strong></span>
+                  </div>
+                  <!-- actions -->
+                  <div class="actions-btns d-flex gap-2 align-items-center me-2">
+                    <strong>{{ sumPrice(item) }}€</strong>
+                    <button class="btn btn-dark" @click="store.addPlate(item.plateObj, restaurant_name)">
+                      <i class="fa-solid fa-plus"></i>
+                    </button>
+                    <span>{{ item.quantity }}x</span>
+                    <button class="btn btn-dark" @click="store.removePlate(item.plateObj)">
+                      <i class="fa-solid fa-minus"></i>
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <div class="total d-flex justify-content-between">
-              <span>Totale Ordine:</span>
-              <strong>{{ store.getTotal() }}€</strong>
+              <div class="total d-flex justify-content-between">
+                <span>Totale Ordine:</span>
+                <strong>{{ store.getTotal() }}€</strong>
+              </div>
             </div>
+          </template>
+
+          <div v-else class="cart">
+            <h2 class="text-center mb-4">Il tuo carrello è vuoto</h2>
+            <button @click="this.$router.go(-1)" class="payment-btn">
+              Continua ad ordinare
+            </button>
           </div>
-        </template>
+        </div>
+        <div class="right col-12 col-lg-6 p-4">
+          <div v-if="store.cart.length > 0" class="card p-4 form-card">
+            <h2 class="mb-3 px-3">Procedi all'ordine</h2>
+            <span class="text-warning fs-5">⚠️Tutti i campi sono obbligatori</span>
+            <form @submit.prevent="sendOrder()" id="payment-form" method="" class="card-form px-3">
+              <div class="mb-3">
+                <label for="customer_name" class="form-label">Nome</label>
+                <input max="50" class="form-control" v-model="customer_name" type="text" name="customer_name"
+                  id="customer_name" />
 
-        <div v-else class="cart">
-          <h2 class="text-center mb-4">Il tuo carrello è vuoto</h2>
-          <button @click="this.$router.go(-1)" class="payment-btn">
-            Continua ad ordinare
-          </button>
+                <div class="text-danger" v-if="errors.customer_name">
+                  {{ errors.customer_name }}
+                </div>
+              </div>
+
+              <div class="mb-3">
+                <label for="customer_lastname" class="form-label">Cognome</label>
+                <input required min="2" max="50" class="form-control" v-model="customer_lastname" type="text"
+                  name="customer_lastname" id="customer_lastname" />
+                <div class="text-danger" v-if="errors.customer_lastname">
+                  {{ errors.customer_lastname }}
+                </div>
+              </div>
+
+              <div class="mb-3">
+                <label for="customer_address" class="form-label">Indirizzo</label>
+                <input class="form-control" v-model="customer_address" type="text" name="customer_address"
+                  id="customer_address" />
+                <div class="text-danger" v-if="errors.customer_address">
+                  {{ errors.customer_address }}
+                </div>
+              </div>
+
+              <div class="mb-3">
+                <label for="customer_phone" class="form-label">Num. di telefono</label>
+                <input required class="form-control" v-model="customer_phone" type="text" name="customer_phone"
+                  id="customer_phone" />
+                <div class="text-danger" v-if="errors.customer_phone">
+                  {{ errors.customer_phone }}
+                </div>
+              </div>
+
+              <div class="mb-3">
+                <label for="customer_email" class="form-label">Email</label>
+                <input class="form-control" v-model="customer_email" type="text" name="customer_email"
+                  id="customer_email" />
+                <div class="text-danger" v-if="errors.customer_email">
+                  {{ errors.customer_email }}
+                </div>
+              </div>
+              <div id="dropin-container"></div>
+              <div class="text-danger fs-5 my-3" v-if="errors.payment_failed">
+                {{ errors.payment_failed }}
+              </div>
+              <p>
+                <strong>Totale: {{ store.getTotal() }}€</strong>
+              </p>
+              <button class="payment-btn" type="submit">Paga</button>
+              <input type="hidden" id="nonce" name="payment_method_nonce" />
+            </form>
+          </div>
         </div>
       </div>
-      <div class="right col-5 p-4">
-        <div v-if="store.cart.length > 0" class="card p-4 form-card">
-          <h2 class="mb-3 px-3">Procedi all'ordine</h2>
-          <span class="text-warning fs-5"
-            >⚠️Tutti i campi sono obbligatori</span
-          >
-          <form
-            @submit.prevent="sendOrder()"
-            id="payment-form"
-            method=""
-            class="card-form px-3"
-          >
-            <div class="mb-3">
-              <label for="customer_name" class="form-label">Nome</label>
-              <input
-                max="50"
-                class="form-control"
-                v-model="customer_name"
-                type="text"
-                name="customer_name"
-                id="customer_name"
-              />
 
-              <div class="text-danger" v-if="errors.customer_name">
-                {{ errors.customer_name }}
-              </div>
-            </div>
-
-            <div class="mb-3">
-              <label for="customer_lastname" class="form-label">Cognome</label>
-              <input
-                required
-                min="2"
-                max="50"
-                class="form-control"
-                v-model="customer_lastname"
-                type="text"
-                name="customer_lastname"
-                id="customer_lastname"
-              />
-              <div class="text-danger" v-if="errors.customer_lastname">
-                {{ errors.customer_lastname }}
-              </div>
-            </div>
-
-            <div class="mb-3">
-              <label for="customer_address" class="form-label">Indirizzo</label>
-              <input
-                class="form-control"
-                v-model="customer_address"
-                type="text"
-                name="customer_address"
-                id="customer_address"
-              />
-              <div class="text-danger" v-if="errors.customer_address">
-                {{ errors.customer_address }}
-              </div>
-            </div>
-
-            <div class="mb-3">
-              <label for="customer_phone" class="form-label"
-                >Num. di telefono</label
-              >
-              <input
-                required
-                class="form-control"
-                v-model="customer_phone"
-                type="text"
-                name="customer_phone"
-                id="customer_phone"
-              />
-              <div class="text-danger" v-if="errors.customer_phone">
-                {{ errors.customer_phone }}
-              </div>
-            </div>
-
-            <div class="mb-3">
-              <label for="customer_email" class="form-label">Email</label>
-              <input
-                class="form-control"
-                v-model="customer_email"
-                type="text"
-                name="customer_email"
-                id="customer_email"
-              />
-              <div class="text-danger" v-if="errors.customer_email">
-                {{ errors.customer_email }}
-              </div>
-            </div>
-            <div id="dropin-container"></div>
-            <div class="text-danger fs-5 my-3" v-if="errors.payment_failed">
-              {{ errors.payment_failed }}
-            </div>
-            <p>
-              <strong>Totale: {{ store.getTotal() }}€</strong>
-            </p>
-            <button class="payment-btn" type="submit">Paga</button>
-            <input type="hidden" id="nonce" name="payment_method_nonce" />
-          </form>
-        </div>
-      </div>
     </div>
   </main>
 </template>
@@ -306,11 +240,9 @@ export default {
 .checkout-main {
   min-height: calc(100vh - 248px);
   background: rgb(250, 89, 0);
-  background: linear-gradient(
-    90deg,
-    rgba(250, 89, 0, 1) 9%,
-    rgba(255, 213, 0, 1) 100%
-  );
+  background: linear-gradient(90deg,
+      rgba(250, 89, 0, 1) 9%,
+      rgba(255, 213, 0, 1) 100%);
   padding-top: 8rem;
   padding-bottom: 4rem;
 
@@ -336,6 +268,7 @@ export default {
       margin-bottom: 20px;
       border-bottom: 1px solid var(--text-dark);
     }
+
     .items {
       overflow-y: auto;
       max-height: 60vh;
@@ -343,6 +276,7 @@ export default {
 
     .item {
       margin-bottom: 20px;
+
       .plate-img {
         margin-right: 15px;
         width: 70px;
@@ -359,6 +293,7 @@ export default {
     }
   }
 }
+
 .payment-btn {
   display: block;
   margin: auto;
@@ -369,6 +304,7 @@ export default {
   color: white;
   font-weight: 600;
   transition: background-color 0.3s ease;
+
   &:hover {
     background-color: #e8590c;
   }
